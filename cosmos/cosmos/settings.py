b9d980de56ad9c2f69aff906bc16fadb35b7ba69
Максим Давлеев <davleevkam@gmail.com>
2023-11-22 14:22:31 +0300

    fixs

--- cosmos/cosmos/settings.py
+++ cosmos/cosmos/settings.py
@@ -118,3 +118,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+THUMBNAIL_DEBUG = True
--- cosmos/page/admin.py
+++ cosmos/page/admin.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
+from django.contrib import admin
 from django.utils.safestring import mark_safe
+from easy_thumbnails.files import get_thumbnailer
 
 from .models import Slider
 
@@ -13,7 +14,7 @@ class SliderAdmin(SortableAdminMixin, admin.ModelAdmin):
         'my_order'
     )
 
+    @admin.display(description='Превью')
     def image_slider(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')
-
-    image_slider.short_description = 'Превью'
+        thumbnailer = get_thumbnailer(obj.image).get_thumbnail({'size': (100, 100), 'crop': 'smart'}).url
+        return mark_safe(f'<img src="{thumbnailer}">')
